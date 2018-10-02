d57c53ad032a3f5b07f9045418af65fb5f734965
severus889700 <severus889700@gmail.com>
2018-10-02 07:41:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,3 +2,12 @@ function forLoop(myArray)
 for (var i = 1; i < 100; i++) {
   console.log("Hello World the " + i + " time");
 }
+
+function whileLoop(number){
+  while (number>0){
+    console.log(number--)
+    if (number=0){
+      return 'done'
+    }
+  }
+}
