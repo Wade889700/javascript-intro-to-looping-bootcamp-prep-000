a39269ae4240b8d90796a0a19121eb18daf022b8
severus889700 <severus889700@gmail.com>
2018-10-02 08:03:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,14 @@
-function forLoop(myArray){
-  for (var i = 0; i < 25; i++){
-  if (i===0){
-    myArray.push("I am 1 strange loop")}
-  else{
-   myArray.push("I am " + i + " strange loops") 
-  }}
-  return myArray
-}
+function forLoop(array){
+   for (var i = 0; i< 25; i++){
+     if(i===0){
+       array.push("I am 1 strange loop");
+     }
+     else {
+     array.push( 'I am ' + i +' strange loops');
+     }
+   }
+   return array ;
+ }
 
 
 function whileLoop(number){
